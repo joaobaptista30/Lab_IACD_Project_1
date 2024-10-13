e9de8d5dc2a113b199feb02cb9ca3b2397653689
João Baptista <joao.baptista@outlook.pt>
2024-10-13 15:58:19 +0100

    Create copy_xml_to_patient.py
    usar para associar o xml com as anotações de cada paciente ao diretorio onde estao as slices dos nodulos

---
+++ copy_xml_to_patient.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import xml.etree.ElementTree as ET
+
+# ================================================================================================
+#       script para associar anotacoes xml ao paciente
+# ================================================================================================
+
+
+#path to directory were CT scans (*.dcm) are
+LIDC_PATH = f"C:/Users/joaob/Documents/LIDC-IDRI/"
+
+#path to directory were xml files are
+XML_PATH = f"C:/Users/joaob/Documents/tcia-lidc-xml/" 
+
+#path to store the processed data
+SAVE_PATH = f'C:/Users/joaob/Documents/LIDC-IDRI-slices/'
+
+
+for xml_f in sorted(os.listdir(XML_PATH)):
+    xml_files = sorted(os.listdir(f"{XML_PATH}{xml_f}"))
+    
+    for file in xml_files:
+        # Load the XML file
+        tree = ET.parse(f"{XML_PATH}{xml_f}/{file}")
+        root = tree.getroot()
+
+        if root.tag == '{http://www.nih.gov/idri}IdriReadMessage': # true for XRs annotations | we only want CTs so just ignore XRs
+            continue
+
+        print(f"XML: {file}")
+        done = False
+        dicom_stack = None
+
+
+        #find the patient (path to folder) related to xml file
+        study_instance_uid = root.find('.//{*}StudyInstanceUID').text #first folder
+        series_instance_uid = root.find('.//{*}SeriesInstanceUid').text #subfolder
+        patient = None
+
+        for patient_name in sorted(os.listdir(f'{LIDC_PATH}')):
+            if done: break
+            for study_uid in sorted(os.listdir(f'{LIDC_PATH}/{patient_name}')):
+
+                if study_uid == study_instance_uid: # .xml file correspond with scans
+                    print(f'paciente: {patient_name}')
+                    patient = patient_name
+                    done = True
+                    shutil.copy(f'{XML_PATH}{xml_f}/{file}', f'{SAVE_PATH}{patient}') #copy .xml file to the respective patient folder
+                    break
